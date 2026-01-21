f08eedef58a8a56bce849e764994b4bcf26300dd
Eldian <eldian.dev@gmail.com>
2026-01-21 11:42:00 +0800

    Set up MongoDB connection

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,20 @@
 import express from "express"; 
 import applicationRoutes from "./routes/applicationRoutes.js"
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv"; 
+
+dotenv.config(); 
 
 const app = express(); 
-const port = 5001; 
+const port = process.env.PORT || 5001; 
+
+connectDB();
 
 app.use("/api/jobs", applicationRoutes); 
 
 app.listen(port, () => {
-    console.log("Server running on PORT: 5001.")
+    console.log("Server running on PORT:", port); 
 }); 
-
 
 
 
